c9c440a256c55a18f576ebc72d8d7a35080e56ec
marlon268 <varon.marlon83@gmail.com>
2021-06-04 16:20:11 -0500

    proyecto

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -4,7 +4,12 @@ import { useState, useEffect } from "react";
 const useCharacters = (url) => {
     const [characters, setCharacters] = useState([]);
     useEffect(() => {
-        fetch(url)
+        fetch(url, {
+            'mode': 'cors',
+	        'headers': {
+            	'Access-Control-Allow-Origin': '*',
+        	}
+        })
           .then((response) => response.json())
           .then((data) => setCharacters(data.results));
       }, [url]);
